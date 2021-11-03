db743b4e9ac3b4ed389771169aec0bde69f75633
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-11-04 01:07:09 +0900

    ID, PW 다 맞는 경우 하나만 틀린 경우 다 틀린 경우 에 따라 스낵바 함수 호출, 페이지 이동, TextEditinController 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dice.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,6 +21,9 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controllerID = TextEditingController();
+  TextEditingController controllerPW = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,64 +36,122 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage('image/calendar.png'),
-                width: 170.0,
-                height: 190.0,
-              ),
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15,
-                    ),
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Center(
+                  child: Image(
+                    image: AssetImage('image/calendar.png'),
+                    width: 170.0,
+                    height: 190.0,
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Enter ID'),
-                        keyboardType: TextInputType.emailAddress,
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15,
+                        ),
                       ),
-                      TextField(
-                        decoration:
-                            InputDecoration(labelText: 'Enter Password'),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      ButtonTheme(
-                          minWidth: 100.0,
-                          height: 50.0,
-                          child: RaisedButton(
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: controllerID,
+                            decoration: InputDecoration(labelText: 'Enter ID'),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            controller: controllerPW,
+                            decoration:
+                                InputDecoration(labelText: 'Enter Password'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          ButtonTheme(
+                            minWidth: 100.0,
+                            height: 50.0,
+                            child: RaisedButton(
                               color: Colors.orangeAccent,
                               child: Icon(
                                 Icons.arrow_forward,
                                 color: Colors.white,
                                 size: 35.0,
                               ),
-                              onPressed: () {}))
-                    ],
+                              onPressed: () {
+                                if (controllerID.text == 'ID' &&
+                                    controllerPW == 'PW') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              Dice()));
+                                } else if (controllerID == 'ID' &&
+                                    controllerPW != 'PW') {
+                                  showSnackBar2(context);
+                                } else if (controllerID != 'ID' &&
+                                    controllerPW == 'PW') {
+                                  showSnackBar3(context);
+                                } else {
+                                  showSnackBar(context);
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '로그인 정보를 다시 확인하세요.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blueGrey,
+  ));
+}
+
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      'PW가 일치하지 않습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blueGrey,
+  ));
+}
+
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      'ID가 일치하지 않습니다.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blueGrey,
+  ));
 }
